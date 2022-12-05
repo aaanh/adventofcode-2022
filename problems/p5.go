ccb41191c51136ef532fca3348cdb5b869a3a04c
Anh Hoang Nguyen <iam@hoanganh.dev>
2022-12-05 13:34:01 -0500

    p5 wip

---
+++ problems/p5.go
@@ -0,0 +1,30 @@
+package problems
+
+func P5() {
+  // Read input file
+  data, err := ioutil.ReadFile("inputs/p5.txt")
+  if err != nil {
+    panic(err)
+  }
+
+  fileContents := string(data)
+
+  arrOfInputs := [
+    "FDBZTJRN",
+    "RSNJH",
+    "CRNJGZFQ",
+    "FVNGRTQ",
+    "LTQF",
+    "QCWZBRGN",
+    "FCLSNHM",
+    "DNQMTJ",
+    "PGS"
+  ]
+
+  var table [][]string
+
+  for i := 0; i <= len(arr) {
+
+  }
+
+}
