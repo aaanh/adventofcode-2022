c1232471b4618fecf623cf8ee394c33c3214674d
Anh Hoang Nguyen <iam@hoanganh.dev>
2022-12-11 19:42:35 -0500

    slow progress but progress nonetheless

--- problems/p5.go
+++ problems/p5.go
@@ -5,6 +5,7 @@ import (
   "strings"
   "io/ioutil"
   "strconv"
+  "reflect"
 )
 
 func parseOperation(s string) []int {
@@ -14,6 +15,10 @@ func parseOperation(s string) []int {
   destin, _ := strconv.Atoi(arrSplitted[5])
 
   return []int{target, origin, destin}
+}
+
+func executeOperation([]int) {
+  
 }
 
 func P5() {
@@ -35,8 +40,30 @@ func P5() {
     "FCLSNHM",
     "DNQMTJ",
     "PGS"}
+
+  letters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+  encoding := map[string]int{}
+
+  operations_str := fileContents[strings.Index(fileContents, "move"):]
+  operations_tmp := strings.Split(operations_str, "\n")
+
+  fmt.Println(reflect.TypeOf(operations_tmp)) // []string
+
+  operations := [][]int{}
+
+  for i := 0; i < len(letters); i++ {
+    encoding[letters[i]] = i + 1
+  }
+
+  for j := 0; j < len(operations_tmp); j++ {
+    operations = append(operations, parseOperation(operations_tmp[j]))
+    
+  }
+
   
 
-  fmt.Println("Problem 5a")
+  fmt.Println(operations)
+  fmt.Println(arrOfInputs)
 
+  // fmt.Println("Problem 5a")
 }
